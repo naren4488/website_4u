1e2843620d491bc3cd932fb111cc289133437f30
4uacademy <4uacademy22@gmail.com>
2023-10-10 12:48:25 +0530

    placement page update

--- src/components/placements/testimonials/placement.js
+++ src/components/placements/testimonials/placement.js
@@ -1,19 +1,17 @@
 import React from "react";
 import "./placement.css";
 import PlacementCard from "./placementCard";
-import placementData from "./leftPlacementData.json"
-import rightPlacementData from "./rightPlacementData.json"
+import placementData from "./leftPlacementData.json";
+import rightPlacementData from "./rightPlacementData.json";
 // all images for placed students
-import shivu_img from "./../../../assets/images/shivaraj.jpeg"
-import chandana_img from "./../../../assets/images/Chandana R.webp"
-import nishant_img from "./../../../assets/images/nishant.jpeg"
-import sumana_img from "./../../../assets/images/sumana.jpeg"
-import kushal_img from "./../../../assets/images/kushal.webp"
-import preethi_img from "./../../../assets/images/preethi.jpg"
-import male_img from "./../../../assets/images/male_avatar.png"
+import shivu_img from "./../../../assets/images/shivaraj.jpeg";
+import chandana_img from "./../../../assets/images/Chandana R.webp";
+import nishant_img from "./../../../assets/images/nishant.jpeg";
+import sumana_img from "./../../../assets/images/sumana.jpeg";
+import kushal_img from "./../../../assets/images/kushal.webp";
+import preethi_img from "./../../../assets/images/preethi.jpg";
+import male_img from "./../../../assets/images/male_avatar.png";
 // import shivu_img from "./../../../assets/images/shiva.jpeg"
-
-
 
 export default function Narendra() {
   // console.log("printing from placement js", placementData);
@@ -29,11 +27,11 @@ export default function Narendra() {
   rightPlacementData[3].image = male_img;
   rightPlacementData[4].image = male_img;
 
-
   // console.log("printing from placement js", placementData);
   return (
     <section className="narendra-card" id="top">
       <div className="top-heading">
+      
         <h2>What we have achieved so far</h2>
         <h4>
           Our purpose is not mere program execution, but rather the precise
@@ -42,15 +40,11 @@ export default function Narendra() {
 
         <div className="top-highlight">
           <div className="highlight-card card-highlight">
-            <p>
-              4 Program
-            </p>
+            <p>4 Program</p>
           </div>
 
           <div className="highlight-card card-highlight">
-            <p>
-              8+ Colleges 
-            </p>
+            <p>8+ Colleges</p>
           </div>
         </div>
 
@@ -65,7 +59,7 @@ export default function Narendra() {
 
           <div className="highlight-card card-highlight">
             <p>
-            8.9 LPA
+              8.9 LPA
               <br />
               Avg. package
             </p>
@@ -74,7 +68,7 @@ export default function Narendra() {
           <div className="highlight-card card-highlight">
             <p>
               18 LPA
-              <br/>
+              <br />
               Highest Package
             </p>
           </div>
@@ -87,19 +81,38 @@ export default function Narendra() {
         <h2>Unbelievable ? Hear from themselves...</h2>
         <div className="placement-grid">
           <div>
-            {placementData.map(({name, salary, review, company, image, linkedIn}) => {
-              // console.log('hello naren', name);
-              return <PlacementCard name={name} salary={salary} review={review} image={image} company={company} linkedIn={linkedIn}/>
-            })}
-            
-            
+            {placementData.map(
+              ({ name, salary, review, company, image, linkedIn }) => {
+                // console.log('hello naren', name);
+                return (
+                  <PlacementCard
+                    name={name}
+                    salary={salary}
+                    review={review}
+                    image={image}
+                    company={company}
+                    linkedIn={linkedIn}
+                  />
+                );
+              }
+            )}
           </div>
           <div>
-          {rightPlacementData.map(({name, salary, review, company, image, linkedIn}) => {
-              // console.log('hello naren', name);
-              return <PlacementCard name={name} salary={salary} review={review} image={image} company={company} linkedIn={linkedIn}/>
-            })}
-           
+            {rightPlacementData.map(
+              ({ name, salary, review, company, image, linkedIn }) => {
+                // console.log('hello naren', name);
+                return (
+                  <PlacementCard
+                    name={name}
+                    salary={salary}
+                    review={review}
+                    image={image}
+                    company={company}
+                    linkedIn={linkedIn}
+                  />
+                );
+              }
+            )}
           </div>
         </div>
       </div>
